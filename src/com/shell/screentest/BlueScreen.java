b1e3496371162ca006f9c5c19d03f165e296cd32
wbzhang <wbzhang@inhuasoft.cn>
2014-08-25 16:42:34 +0800

    Demo work OK

--- src/com/shell/screentest/BlueScreen.java
+++ src/com/shell/screentest/BlueScreen.java
@@ -1,13 +1,37 @@
 package com.shell.screentest;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.BaseDrawable;
 
 public class BlueScreen implements Screen {
 
+	private MyGame game;
+	private Stage stage;
+	private Group gameNode;
+
+	private Image bg;
+	private SpriteBatch batch;
+	private int width, heigth;
+
+	
 	@Override
 	public void render(float delta) {
 		// TODO Auto-generated method stub
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+		this.stage.act(delta);
+		this.stage.draw();
 	}
 
 	@Override
@@ -19,13 +43,45 @@ public class BlueScreen implements Screen {
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
+		this.game = (MyGame) Gdx.app.getApplicationListener();
+		this.stage = this.game.stage;
+		this.gameNode = this.game.gameNode;
+		batch = new SpriteBatch();
+		width = Gdx.graphics.getWidth();
+		heigth = Gdx.graphics.getHeight();
 
+		batch.setColor(Color.BLUE);
+//		BaseDrawable drawable = new BaseDrawable();
+//		drawable.draw(batch, 0, 0, width, heigth);
+		
+		Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+		pixmap.setColor( 0, 0, 1, 1.0f );
+		pixmap.fillRectangle(0, 0, 1, 1);
+		
+		bg = new Image(new Texture(pixmap));
+		pixmap.dispose();
+		bg.setPosition(0, 0);
+		bg.setBounds(0, 0, width, heigth);
+		this.gameNode.addActor(bg);
+		bg.addListener(new InputListener() {
+
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+				// TODO Auto-generated method stub
+				System.out.println("wbzhang   --------------   BlueScreen touchDown");
+				BlueScreen.this.game.setScreen(BlueScreen.this.game.screen1);
+				return false;
+			}
+
+		});
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-
+		this.gameNode.clear();
+		dispose();
 	}
 
 	@Override
@@ -43,7 +99,7 @@ public class BlueScreen implements Screen {
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-
+		batch.dispose();
 	}
 
 }
--- src/com/shell/screentest/MyGame.java
+++ src/com/shell/screentest/MyGame.java
@@ -1,19 +1,38 @@
 package com.shell.screentest;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class MyGame extends Game {
 
 	public Stage stage;
+	public Group gameNode;
 	public AssetManager manager;
+	
+	public RedScreen screen1;
+	public GreenScreen screen2;
+	public BlueScreen screen3;
 
 	@Override
 	public void create() {
 		// TODO Auto-generated method stub
+		if (this.stage != null)
+			return;
 		stage = new Stage();
+		stage.getRoot().setTransform(false);
+		Gdx.input.setInputProcessor(stage);
+		gameNode = new Group();
+		this.stage.addActor(gameNode);
 		manager = new AssetManager();
+		
+		screen1 = new RedScreen();
+		screen2 = new GreenScreen();
+		screen3 = new BlueScreen();
+		
+		this.setScreen(screen1);
 	}
 
 	@Override
